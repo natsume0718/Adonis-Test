05b5298c4ace74a2cc6f7e29e9def5d5d12ad2fa
natsume0718 <moto718program@gmail.com>
2021-08-06 02:56:13 +0900

    use puppeteer

--- app/Controllers/AuthController.ts
+++ app/Controllers/AuthController.ts
@@ -1,7 +1,17 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import puppeteer from 'puppeteer-core'
 
 export default class AuthController {
   public async check(ctx: HttpContextContract) {
-    return []
+    const br = await puppeteer.launch({
+      // update 2021/08/05
+      channel: 'chrome',
+      args: ['--no-sandbox', '--disable-dev-shm-usage'],
+    })
+    const page = await br.newPage()
+    const response = await page.goto('https://google.com')
+    return {
+      response,
+    }
   }
 }
